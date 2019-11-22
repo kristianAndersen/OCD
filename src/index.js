030beb6f6da988d6ee47561ec85fabe788abe5af
kristian andersen <krilleandersen@gmail.com>
2019-11-22 13:43:21 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,144 @@
+const {app, BrowserWindow, ipcMain, Tray} = require('electron');
+const path = require('path');
+
+let tray = undefined
+let window = undefined
+
+// Don't show the app in the doc
+//app.dock.hide()
+
+app.on('ready', () => {
+  createTray()
+  createWindow()
+})
+
+const createTray = () => {
+  tray = new Tray(path.join(__dirname+'/ocdicon_white.png'))
+  tray.on('click', function (event) {
+    toggleWindow()
+  });
+}
+
+const getWindowPosition = () => {
+  const windowBounds = window.getBounds();
+  const trayBounds = tray.getBounds();
+
+  // Center window horizontally below the tray icon
+  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2));
+
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 4);
+
+  return {x: x, y: y};
+}
+
+const createWindow = () => {
+  window = new BrowserWindow({
+    width: 320,
+    height: 650,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: false,
+    webPreferences: {
+      backgroundThrottling: false
+    }
+  })
+  window.loadURL(`file://${__dirname}/index.html`);
+  // Hide the window when it loses focus
+  window.on('blur', () => {
+    if (!window.webContents.isDevToolsOpened()) {
+      window.hide()
+    }
+  })
+}
+
+const toggleWindow = () => {
+  window.isVisible() ? window.hide() : showWindow();
+}
+
+const showWindow = () => {
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y, false);
+  window.show();
+}
+
+ipcMain.on('show-window', () => {
+  showWindow()
+})
+/*
+const { app, BrowserWindow, Menu, Tray } = require('electron');
+
+
+
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 22,
+    height: 22,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+//app.on('ready', createWindow);
+
+let tray = null
+app.on('ready', () => {
+  tray = new Tray(__dirname+'/ocdicon_white.png')
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+  tray.setToolTip('This is my application.')
+  tray.setContextMenu(contextMenu)
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+*/
